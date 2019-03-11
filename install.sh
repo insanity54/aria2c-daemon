155e905fe00132fc84c09be83e11c3dcead1ff5d
Chris Grimmett <chris@grimtech.net>
2019-03-10 22:35:45 -0700

    enable service

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo mkdir -p /usr/local/etc/aria2
 sudo cp ./aria2c.service /etc/systemd/system/
 sudo cp ./aria2.conf /usr/local/etc/aria2/aria2.conf
 sudo systemctl daemon-reload
+sudo systemctl enable aria2c
